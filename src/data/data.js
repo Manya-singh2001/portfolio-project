30a88eff1b9fa437c32ff53f4ff326d6204d36e2
Manya-singh2001 <manyasinghsingh16699@gmail.com>
2023-04-20 17:56:26 +0530

    add work and contact

---
+++ src/data/data.js
@@ -0,0 +1,56 @@
+import defiWEB from '../assets/defi-website.png'
+import fastFood from '../assets/fast-food-app.png'
+import weekaway from '../assets/weekaway-website.png'
+import netflix from '../assets/netflix-clone.png'
+import headset from '../assets/vr-headset.png'
+import cloudWeb from '../assets/cloud-web.png'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:defiWEB,
+        github:"https://github.com/Manya-singh2001/defi-app",
+        live:"https://manya-singh2001.github.io/defi-app/",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:headset,
+        github:"https://github.com/Manya-singh2001/vr-headset-website",
+        live:"https://manya-singh2001.github.io/vr-headset-website/",
+    },
+   
+    {
+        id:3,
+        name:"React JS Application",
+        image:netflix,
+        github:"",
+        live:"",
+    },
+   
+    {
+        id:4,
+        name:"React JS Application",
+        image:fastFood,
+        github:"https://github.com/Manya-singh2001/Fastfood-App",
+        live:"https://manya-singh2001.github.io/Fastfood-App/",
+    },
+ {
+        id:5,
+        name:"React JS Application",
+        image:cloudWeb,
+        github:"https://github.com/Manya-singh2001/brand-website",
+        live:"https://manya-singh2001.github.io/brand-website/",
+    },
+    {
+        id:6,
+        name:"React JS Application",
+        image:weekaway,
+        github:"",
+        live:"",
+    }
+    
+
+
+]
